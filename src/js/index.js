5523274a38a703502a470cbb96aa7c32e5c5e80c
pikktorr <viktor.rozgonyi@gmail.com>
2019-12-01 12:34:30 +0100

    back-to-top

--- src/js/index.js
+++ src/js/index.js
@@ -1,68 +1,18 @@
-// const projects = [
-//   {
-//     title: "Smart-Brain",
-//     tool: "React.js, PostgreSQL",
-//     about:
-//       "This application detects faces on pasted images and counts how many times you used it with the registered profile.",
-//     link: ""
-//   },
-//   {
-//     title: "RoboFriends",
-//     tool: "React.js",
-//     about: "Type in names to get an instant search result.",
-//     link: ""
-//   },
-//   {
-//     title: "Carousel",
-//     tool: "Vanilla JavaScript",
-//     about:
-//       "A vanilla javascript carousel with my favourite music albums. Alt-J, Beck, John Coffey, Portugal the Man, Radiohead, Red Hot Chili Peppers, Slipknot, Tool",
-//     link: ""
-//   },
-//   {
-//     title: "The Shopping List",
-//     tool: "JavaScript",
-//     about:
-//       "A simple, mandatory shopping list with random colored item backgrounds. Everything we need!",
-//     link: ""
-//   },
-//   {
-//     title: "UX/UI Design",
-//     tool: "Adobe XD",
-//     about:
-//       "A mobile portfolio design from hand drawn sketches to wireframes to prototype preview.",
-//     link:
-//       "https://xd.adobe.com/view/f7600bf5-e88a-4c24-4472-e9ecc2d04c0c-8151/?fullscreen"
-//   },
-//   {
-//     title: "Outdoor stage in Szeged",
-//     tool: "ArchiCAD, Photoshop, InDesign, Illustrator",
-//     about:
-//       "This work was my Master Architecture Degree project. An outdoor stage in Liget park in Szeged... ",
-//     link:
-//       "https://xd.adobe.com/view/f7600bf5-e88a-4c24-4472-e9ecc2d04c0c-8151/?fullscreen"
-//   }
-// ];
+//NAVIGATION - BACK TO TOP
 
-// const skills = [
-//   {
-//     image: "archifitty",
-//     name: "ArchiCAD",
-//     url: "https://www.graphisoft.hu/archicad/"
-//   },
-//   {
-//     image: "vsfitty",
-//     name: "VS Code"
-//   },
-//   {
-//     image: "ps",
-//     name: "Photoshop"
-//   }
-// ];
+const topButton = document.querySelector(".back-to-top");
+const scrollFunction = () => {
+  document.body.scrollTop > 200 || document.documentElement.scrollTop > 200
+    ? (topButton.style.visibility = "visible")
+    : (topButton.style.visibility = "hidden");
+};
+window.onscroll = () => scrollFunction();
+
+topButton.onclick = () => window.scrollTo(0, 0);
 
 // NAVIGATION - SCROLL
 
-import {skills} from "./skills";
+import { skills } from "./skills";
 
 const scrollPage = targetId => {
   const target = document.querySelector(targetId);
@@ -87,11 +37,6 @@ navToSections.map(element => {
     scrollPage(element.section);
   });
 });
-
-// const skillsButton = document.querySelector("#nav-skills");
-// skillsButton.addEventListener("click", () => {
-//   scrollPage("#skills-section");
-// });
 
 // PROJECTS
 
