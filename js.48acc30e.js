2107529c6b1f870ff868dce3bf5fbd0a6ec04671
pikktorr <viktor.rozgonyi@gmail.com>
2020-01-08 19:24:36 +0100

    Updates

---
+++ js.48acc30e.js
@@ -0,0 +1,118 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"WWtL":[function(require,module,exports) {
+module.exports="/smartbrainer_small.f774c849.jpg";
+},{}],"k9wx":[function(require,module,exports) {
+module.exports="/robofriends_small.9e328b17.png";
+},{}],"O68T":[function(require,module,exports) {
+module.exports="/cant-do-it-all-small.e42ef917.png";
+},{}],"qGMk":[function(require,module,exports) {
+module.exports="/carousel_small.d3039383.png";
+},{}],"EvD7":[function(require,module,exports) {
+module.exports="/shopping_small.e9c05125.jpg";
+},{}],"k9Gk":[function(require,module,exports) {
+module.exports="/ux_ui_small.155fe516.png";
+},{}],"Je2d":[function(require,module,exports) {
+module.exports="/msc-1.11655a3f.png";
+},{}],"FwO4":[function(require,module,exports) {
+module.exports="/msc-2.88bb8647.png";
+},{}],"n4Mn":[function(require,module,exports) {
+module.exports="/msc-3.787ff200.png";
+},{}],"Sjdt":[function(require,module,exports) {
+module.exports="/msc-4.91eba66c.png";
+},{}],"fC1H":[function(require,module,exports) {
+module.exports="/msc-5.6f3d5752.png";
+},{}],"EREN":[function(require,module,exports) {
+module.exports="/bsc-1.3a5a4db3.jpg";
+},{}],"xab6":[function(require,module,exports) {
+module.exports="/bsc-2.e9294ae5.jpg";
+},{}],"jhfT":[function(require,module,exports) {
+module.exports="/bsc-3.933f0e65.jpg";
+},{}],"X0cD":[function(require,module,exports) {
+module.exports="/bsc-4.6f29d715.jpg";
+},{}],"Zr5y":[function(require,module,exports) {
+module.exports="/bsc-5.34275312.jpg";
+},{}],"huIq":[function(require,module,exports) {
+module.exports="/digital-1.50055a82.png";
+},{}],"MNpB":[function(require,module,exports) {
+module.exports="/digital-2.20e72b67.png";
+},{}],"rM0A":[function(require,module,exports) {
+module.exports="/digital-3.7946b2f4.jpg";
+},{}],"lDAH":[function(require,module,exports) {
+module.exports="/digital-4.49fe80ee.png";
+},{}],"FN0i":[function(require,module,exports) {
+module.exports="/digital-5.695091bd.png";
+},{}],"MILG":[function(require,module,exports) {
+module.exports="/sketch-1.03dbf1b1.png";
+},{}],"whf9":[function(require,module,exports) {
+module.exports="/sketch-2.b9490346.png";
+},{}],"biTO":[function(require,module,exports) {
+module.exports="/sketch-3.3aa236e0.png";
+},{}],"vcnY":[function(require,module,exports) {
+module.exports="/sketch-4.1cd78730.png";
+},{}],"gvzY":[function(require,module,exports) {
+module.exports="/sketch-5.e49a599a.png";
+},{}],"pWkg":[function(require,module,exports) {
+module.exports="/3dsmax.f8cdddf7.png";
+},{}],"efbJ":[function(require,module,exports) {
+module.exports="/adobexd.b87f5bb7.png";
+},{}],"ToHw":[function(require,module,exports) {
+module.exports="/Archicad.a854df09.png";
+},{}],"neTm":[function(require,module,exports) {
+module.exports="/bootstrap.1968d704.png";
+},{}],"tP8d":[function(require,module,exports) {
+module.exports="/cinema4d.bfc8300d.jpg";
+},{}],"CYAL":[function(require,module,exports) {
+module.exports="/commandline.ddd412a0.png";
+},{}],"ncUn":[function(require,module,exports) {
+module.exports="/css.8ebccb73.png";
+},{}],"C5Dl":[function(require,module,exports) {
+module.exports="/es6.b37b7269.png";
+},{}],"zDGL":[function(require,module,exports) {
+module.exports="/figma.d1694421.png";
+},{}],"oR1H":[function(require,module,exports) {
+module.exports="/git.5e439629.png";
+},{}],"yHH5":[function(require,module,exports) {
+module.exports="/github.569a1cc1.png";
+},{}],"iDYa":[function(require,module,exports) {
+module.exports="/google.86de58b0.png";
+},{}],"fbBB":[function(require,module,exports) {
+module.exports="/heroku.91e2b480.png";
+},{}],"GrSJ":[function(require,module,exports) {
+module.exports="/html.e5a4a936.png";
+},{}],"ebqy":[function(require,module,exports) {
+module.exports="/illustrator.91fdb779.png";
+},{}],"g4Ct":[function(require,module,exports) {
+module.exports="/Indesign.1d704a3f.png";
+},{}],"gJBa":[function(require,module,exports) {
+module.exports="/javascript.e28186a5.png";
+},{}],"rMlV":[function(require,module,exports) {
+module.exports="/mdn.e3c5f35b.png";
+},{}],"U83T":[function(require,module,exports) {
+module.exports="/nodejs.e75cd47f.png";
+},{}],"Jpos":[function(require,module,exports) {
+module.exports="/npm.307144f5.png";
+},{}],"VksN":[function(require,module,exports) {
+module.exports="/parceljs.19606775.png";
+},{}],"ud29":[function(require,module,exports) {
+module.exports="/photoshop.a3e91ac4.png";
+},{}],"kEod":[function(require,module,exports) {
+module.exports="/postgresql.59195ee4.png";
+},{}],"Ra3v":[function(require,module,exports) {
+module.exports="/postman.1c40292a.png";
+},{}],"mBp4":[function(require,module,exports) {
+module.exports="/Procreate.6f58614b.png";
+},{}],"zCX9":[function(require,module,exports) {
+module.exports="/Reactjs.b40ce8a0.png";
+},{}],"VClY":[function(require,module,exports) {
+module.exports="/Sass.c5ea9ad9.png";
+},{}],"cKjE":[function(require,module,exports) {
+module.exports="/sketchup.ac2fa6c8.png";
+},{}],"Rrbc":[function(require,module,exports) {
+module.exports="/trello.63c83670.png";
+},{}],"xtQr":[function(require,module,exports) {
+module.exports="/vscode.5affd5cd.png";
+},{}],"p56V":[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.skills=exports.gallery=exports.projects=void 0;var e=[{title:"Smart-Brain",tool:"React.js, PostgreSQL",about:"This application detects faces on pasted images and counts how many times you used it with the registered profile.",image:require("../images/projects/smartbrainer_small.jpg"),link:"https://smart-brainer-by-pikktorr.herokuapp.com/"},{title:"RoboFriends",tool:"React.js",about:"Type in names to get an instant search result.",image:require("../images/projects/robofriends_small.png"),link:"https://pikktorr.github.io/robofriends/"},{title:"Can't Do It All",tool:"Javascript",about:"One have to choose between coding, social life and sleeping. Can't do it all...",image:require("../images/projects/cant-do-it-all-small.png"),link:"https://pikktorr.github.io/cant-do-it-all/"},{title:"Infinite Carousel",tool:"JavaScript",about:"A vanilla javascript infinite carousel with my favourite music albums. Alt-J, Beck, John Coffey, Portugal the Man, Radiohead, Red Hot Chili Peppers, Slipknot, Tool",image:require("../images/projects/carousel_small.png"),link:"https://pikktorr.github.io/vanilla-carousel/"},{title:"The Shopping List",tool:"JavaScript",about:"A simple, mandatory shopping list with random colored item backgrounds. Everything you need!",image:require("../images/projects/shopping_small.jpg"),link:"https://pikktorr.github.io/shopping-list.github.io/"},{title:"UX/UI Portfolio Design",tool:"Adobe XD",about:"A mobile portfolio design from hand drawn sketches to wireframes to prototype preview.",image:require("../images/projects/ux_ui_small.png"),link:"https://xd.adobe.com/view/f7600bf5-e88a-4c24-4472-e9ecc2d04c0c-8151/?fullscreen"}];exports.projects=e;var i=[{image:require("../images/gallery/msc-1.png"),title:"Outdoor Stage in Szeged",about:"Master Architecture Degree project"},{image:require("../images/gallery/msc-2.png"),title:"Installation in the Liget Park",about:"Master Architecture Degree Project"},{image:require("../images/gallery/msc-3.png"),title:"Floor Plans and Detailed Section",about:"Master Architecture Degree Project"},{image:require("../images/gallery/msc-4.png"),title:"Sections and Facades",about:"Master Architecture Degree Project"},{image:require("../images/gallery/msc-5.png"),title:"Day/Night Visuals",about:"Master Architecture Degree Project"},{image:require("../images/gallery/bsc-1.jpg"),title:"Musical Instruments Workshop in Pécs",about:"Architectural Engineering Degree Project"},{image:require("../images/gallery/bsc-2.jpg"),title:"Street View of the Workshop",about:"Architectural Engineering Degree Project"},{image:require("../images/gallery/bsc-3.jpg"),title:"Floor Plans and Sections",about:"Architectural Engineering Degree Project"},{image:require("../images/gallery/bsc-4.jpg"),title:"The Show-Window from the Street",about:"Architectural Engineering Degree Project"},{image:require("../images/gallery/bsc-5.jpg"),title:"Inner Showroom ",about:"Architectural Engineering Degree Project"},{image:require("../images/gallery/digital-1.png"),title:"Wolf Medallion from The Witcher",about:"Digital Art"},{image:require("../images/gallery/digital-2.png"),title:"Need Potion",about:"Digital Art"},{image:require("../images/gallery/digital-3.jpg"),title:"Messing with Watercolour",about:"Digital Art"},{image:require("../images/gallery/digital-4.png"),title:"Oh Deer!",about:"Digital Art"},{image:require("../images/gallery/digital-5.png"),title:"Concrete Forest",about:"Digital Art"},{image:require("../images/gallery/sketch-1.png"),title:"By the Lake",about:"Sketches"},{image:require("../images/gallery/sketch-2.png"),title:"Backend Firewalls",about:"Sketches"},{image:require("../images/gallery/sketch-3.png"),title:"Móra Ferenc Museum, Szeged",about:"Sketches"},{image:require("../images/gallery/sketch-4.png"),title:"Anna-Well, Szeged",about:"Sketches"},{image:require("../images/gallery/sketch-5.png"),title:"It's a Tree",about:"Sketches"}];exports.gallery=i;var a=[{image:require("../images/skills/3dsmax.png"),name:"3ds Max"},{image:require("../images/skills/adobexd.png"),name:"Adobe Xd"},{image:require("../images/skills/Archicad.png"),name:"ArchiCAD"},{image:require("../images/skills/bootstrap.png"),name:"Bootstrap"},{image:require("../images/skills/cinema4d.jpg"),name:"Cinema 4D"},{image:require("../images/skills/commandline.png"),name:"Command Line"},{image:require("../images/skills/css.png"),name:"CSS3"},{image:require("../images/skills/es6.png"),name:"ECMAScript6"},{image:require("../images/skills/figma.png"),name:"Figma"},{image:require("../images/skills/git.png"),name:"Git"},{image:require("../images/skills/github.png"),name:"Github"},{image:require("../images/skills/google.png"),name:"HowToGoogleThings"},{image:require("../images/skills/heroku.png"),name:"Heroku"},{image:require("../images/skills/html.png"),name:"HTML5"},{image:require("../images/skills/illustrator.png"),name:"Illustrator"},{image:require("../images/skills/Indesign.png"),name:"Indesign"},{image:require("../images/skills/javascript.png"),name:"Javascript"},{image:require("../images/skills/mdn.png"),name:"MDN Webdocs"},{image:require("../images/skills/nodejs.png"),name:"Node.js"},{image:require("../images/skills/npm.png"),name:"npm"},{image:require("../images/skills/parceljs.png"),name:"Parcel.js"},{image:require("../images/skills/photoshop.png"),name:"Photoshop"},{image:require("../images/skills/postgresql.png"),name:"PostgreSQL"},{image:require("../images/skills/postman.png"),name:"Postman"},{image:require("../images/skills/Procreate.png"),name:"Procreate"},{image:require("../images/skills/Reactjs.png"),name:"React.js"},{image:require("../images/skills/Sass.png"),name:"Sass"},{image:require("../images/skills/sketchup.png"),name:"Sketchup"},{image:require("../images/skills/trello.png"),name:"Trello"},{image:require("../images/skills/vscode.png"),name:"VS Code"}];exports.skills=a;
+},{"../images/projects/smartbrainer_small.jpg":"WWtL","../images/projects/robofriends_small.png":"k9wx","../images/projects/cant-do-it-all-small.png":"O68T","../images/projects/carousel_small.png":"qGMk","../images/projects/shopping_small.jpg":"EvD7","../images/projects/ux_ui_small.png":"k9Gk","../images/gallery/msc-1.png":"Je2d","../images/gallery/msc-2.png":"FwO4","../images/gallery/msc-3.png":"n4Mn","../images/gallery/msc-4.png":"Sjdt","../images/gallery/msc-5.png":"fC1H","../images/gallery/bsc-1.jpg":"EREN","../images/gallery/bsc-2.jpg":"xab6","../images/gallery/bsc-3.jpg":"jhfT","../images/gallery/bsc-4.jpg":"X0cD","../images/gallery/bsc-5.jpg":"Zr5y","../images/gallery/digital-1.png":"huIq","../images/gallery/digital-2.png":"MNpB","../images/gallery/digital-3.jpg":"rM0A","../images/gallery/digital-4.png":"lDAH","../images/gallery/digital-5.png":"FN0i","../images/gallery/sketch-1.png":"MILG","../images/gallery/sketch-2.png":"whf9","../images/gallery/sketch-3.png":"biTO","../images/gallery/sketch-4.png":"vcnY","../images/gallery/sketch-5.png":"gvzY","../images/skills/3dsmax.png":"pWkg","../images/skills/adobexd.png":"efbJ","../images/skills/Archicad.png":"ToHw","../images/skills/bootstrap.png":"neTm","../images/skills/cinema4d.jpg":"tP8d","../images/skills/commandline.png":"CYAL","../images/skills/css.png":"ncUn","../images/skills/es6.png":"C5Dl","../images/skills/figma.png":"zDGL","../images/skills/git.png":"oR1H","../images/skills/github.png":"yHH5","../images/skills/google.png":"iDYa","../images/skills/heroku.png":"fbBB","../images/skills/html.png":"GrSJ","../images/skills/illustrator.png":"ebqy","../images/skills/Indesign.png":"g4Ct","../images/skills/javascript.png":"gJBa","../images/skills/mdn.png":"rMlV","../images/skills/nodejs.png":"U83T","../images/skills/npm.png":"Jpos","../images/skills/parceljs.png":"VksN","../images/skills/photoshop.png":"ud29","../images/skills/postgresql.png":"kEod","../images/skills/postman.png":"Ra3v","../images/skills/Procreate.png":"mBp4","../images/skills/Reactjs.png":"zCX9","../images/skills/Sass.png":"VClY","../images/skills/sketchup.png":"cKjE","../images/skills/trello.png":"Rrbc","../images/skills/vscode.png":"xtQr"}],"QvaY":[function(require,module,exports) {
+"use strict";var e=require("./lists");window.addEventListener("orientationchange",function(){return location.reload()});var n=document.querySelector(".back-to-top"),t=function(){document.body.scrollTop>600||document.documentElement.scrollTop>600?n.style.visibility="visible":n.style.visibility="hidden"};window.onscroll=function(){return t()},n.onclick=function(){return window.scrollTo(0,0)};var c=document.querySelector(".bg-logo"),o=document.querySelector(".nav-container"),i=document.querySelector(".nav-background"),r=document.querySelector(".nav-menu"),a=document.querySelector(".menu-close"),l=function(){o.style.display="grid",setTimeout(function(){i.classList.add("menu-open")},10),setTimeout(function(){c.style.display="none"},100),setTimeout(function(){r.style.display="flex"},300)},s=function(){r.style.display="none",i.classList.remove("menu-open"),setTimeout(function(){c.style.display="flex"},200),setTimeout(function(){o.style.display="none"},300)};c.addEventListener("click",function(){window.innerWidth<window.innerHeight&&l()}),a.addEventListener("click",function(){return s()});var d=function(e){var n=document.querySelector(e),t=n.getBoundingClientRect().left,c=n.getBoundingClientRect().top;window.scrollTo(t,c)},u=[{nav:"#nav-home",section:"#home-section"},{nav:"#nav-about",section:"#about-section"},{nav:"#nav-projects",section:"#projects-section"},{nav:"#nav-slide",section:"#slide-section"},{nav:"#nav-skills",section:"#skills-section"},{nav:"#nav-contact",section:"#contact-section"}];u.map(function(e){document.querySelector(e.nav).addEventListener("click",function(){d(e.section),window.innerWidth<window.innerHeight&&s()})});var m=document.querySelector(".projectList");e.projects.map(function(e){var n=document.createElement("article");return n.className="project-container",n.innerHTML='\n    <div class="project-image">\n      <img src='.concat(e.image,"  alt=").concat(e.title.replace(/\s/g,""),' />\n      </div>\n      <div class="project-text">\n        <h2 class="project-title">').concat(e.title,'</h2>\n        <p class="project-tool">(').concat(e.tool,")</p>\n        <hr />\n        <p>").concat(e.about,"</p>\n      </div>\n  "),n.addEventListener("click",function(){return window.open(e.link)}),m.appendChild(n)});var v=0,y=document.querySelectorAll(".slide-container>img"),p=document.querySelectorAll(".dot"),g=function e(){y.forEach(function(e,n){e.classList.add("hidden-img")}),v>=y.length&&(v=0),y.forEach(function(e){return e.classList.remove("active-img")}),y[v].classList.replace("hidden-img","active-img"),p.forEach(function(e){return e.classList.remove("active")}),p[v].classList.add("active"),v++,setTimeout(e,4e3)};g(),y.forEach(function(e,n){return e.addEventListener("click",function(){switch(S(),n){case 0:T(0);break;case 1:T(5);break;case 2:T(10);break;default:T(0)}})});var f=document.querySelector(".gallery-content"),h=document.querySelector(".modal-container"),k=document.querySelector(".close-button"),w=document.querySelector(".prev-img"),b=document.querySelector(".next-img"),L=0,q=e.gallery.map(function(n,t){var c=document.createElement("div");return c.className="gallery-image active-img",c.innerHTML="\n  <img src=".concat(n.image,'>\n  <div class="img-caption">\n    <p>').concat(t+1,"/").concat(e.gallery.length,'</p>\n    <h3 class="img-title">').concat(n.title,"</h3>\n    <p>").concat(n.about,"</p>\n    </div>\n    "),c}),S=function(){h.style.display="block",document.body.style.overflowY="hidden"},E=function(){h.style.display="none",q.map(function(e){return e.remove()}),document.body.style.overflowY=""},T=function(e){j(L=e)},j=function(e){q.map(function(e){return e.remove()}),e>=q.length&&(L=0),e<0&&(L=q.length-1),f.appendChild(q[L])},C=function(e){j(L+=e)};b.addEventListener("click",function(){return C(1)}),document.addEventListener("keydown",function(e){39===e.which&&"block"===h.style.display&&C(1)}),w.addEventListener("click",function(){return C(-1)}),document.addEventListener("keydown",function(e){37===e.which&&"block"===h.style.display&&C(-1)}),k.addEventListener("click",E),document.addEventListener("keydown",function(e){27===e.which&&"block"===h.style.display&&E()});var H=document.querySelector(".skills-badges");e.skills.map(function(e,n){var t=document.createElement("article");return t.className="badge skill".concat(n),t.innerHTML='\n  <div class="badge-img" title='.concat(e.name.replace(/\s/g,""),">\n    <img src=").concat(e.image,"  alt=").concat(e.name.replace(/\s/g,"")," /></div>\n  "),H.appendChild(t)});var x=document.querySelector(".email"),M=document.querySelector(".github"),N=document.querySelector(".linkedin"),z=document.querySelector(".twitter"),A=document.querySelector(".instagram"),B=function(e,n){e.addEventListener("click",function(){window.open(n,"_blank")})};B(x,"mailto:viktor.rozgonyi@gmail.com"),B(M,"https://github.com/pikktorr"),B(N,"https://www.linkedin.com/in/viktor-rozgonyi/"),B(z,"https://twitter.com/pikktorr"),B(A,"https://www.instagram.com/pikktorr/");
+},{"./lists":"p56V"}]},{},["QvaY"], null)
+//# sourceMappingURL=/js.48acc30e.js.map
